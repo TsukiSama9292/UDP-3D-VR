e6164e887cf0d17806a9025471cbc5be84be25ce
TsukiSama9292 <a0985821880@gmail.com>
2024-11-16 14:36:22 +0800

    特效

--- Assets/Scripts/WeaponSystem.cs
+++ Assets/Scripts/WeaponSystem.cs
@@ -10,6 +10,18 @@ namespace tsuki{
         private GameObject prefabBullet;
         [SerializeField, Header("子彈速度"), Range(0,2000)]
         private int bulletSpeed = 1000;
+        [SerializeField, Header("開槍特效")]
+        private ParticleSystem psFire;
+        [SerializeField, Header("開槍音效")]
+        private AudioClip soundFire;
+        private AudioSource aud;
+        
+        private void Awake(){
+            aud = GetComponent<AudioSource>();
+        }
+        /// <summary>
+        /// 發射子彈
+        /// </sumary>
         public void Fire()
         {
             GameObject temp = Instantiate(prefabBullet, point.position , Quaternion.identity);
